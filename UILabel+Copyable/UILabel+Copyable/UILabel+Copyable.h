eae4806cc2963f5bbd6bf41aff2bb78f58632a02
sichenwang <52870104@qq.com>
2016-02-19 00:29:32 +0800

    add comments

--- UILabel+Copyable/UILabel+Copyable/UILabel+Copyable.h
+++ UILabel+Copyable/UILabel+Copyable/UILabel+Copyable.h
@@ -10,8 +10,14 @@
 
 @interface UILabel (Copyable)
 
+/**
+ *  是否支持长按复制，默认为NO
+ */
 @property (nonatomic, assign)IBInspectable BOOL copyingEnabled;
 
+/**
+ *  自定义复制的文本，如果该值为nil，则复制默认的text
+ */
 @property (nonatomic, copy) NSString *copyingText;
 
 @end
--- UILabel+Copyable/ViewController.m
+++ UILabel+Copyable/ViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "ViewController.h"
-#import "UILabel+Copyable.h"
 
 @interface ViewController ()
 
